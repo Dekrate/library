28563e7b487233a55c73c21973abe29213648626
Dekrate <51668658+Dekrate@users.noreply.github.com>
2019-06-10 18:01:44 +0200

    0.2
    - Added new class Books.
    - Every book from now will be an object.

---
+++ src/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    String title;
+    String author;
+    short releaseDate;
+    short pages;
+    String publisher;
+    String isbn;
+}
--- src/Library.java
+++ src/Library.java
@@ -2,21 +2,22 @@ public class Library {
     public static void main(String[] args) {
         final String appName = "Biblioteka";
 
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int releaseDate = 2010;
-        int pages = 256;
-        String publisher = "Greg";
-        String isbn = "9788373278190";
+        Book book1 = new Book();
+        book1.title = "W pustyni i w puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.releaseDate = 2010;
+        book1.pages = 256;
+        book1.publisher = "Greg";
+        book1.isbn = "9788373278190";
 
         System.out.println(appName);
         System.out.println("Książki w bibliotece");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
 
     }
 }
