8ed4f353e5d0e63affed65117ec77021a9a6b109
Dekrate <51668658+Dekrate@users.noreply.github.com>
2019-06-10 17:57:15 +0200

    0.1
    The very first version of library.
    - Shows informations about book "W pustyni i w puszczy" of Henryk Sienkiewicz (author, title, ISBN, amount of pages, publisher, release date).

---
+++ src/Library.java
@@ -0,0 +1,22 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka";
+
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseDate = 2010;
+        int pages = 256;
+        String publisher = "Greg";
+        String isbn = "9788373278190";
+
+        System.out.println(appName);
+        System.out.println("Książki w bibliotece");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+
+    }
+}
