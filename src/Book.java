533844ea4d2e0216445940c9e2d323d2e67584fd
Dekrate <51668658+Dekrate@users.noreply.github.com>
2019-07-01 20:01:00 +0200

    0.3
    - Added constructors.
    - New argument - binding.

--- src/Book.java
+++ src/Book.java
@@ -1,8 +1,28 @@
 public class Book {
     String title;
     String author;
-    short releaseDate;
-    short pages;
+    int releaseDate;
+    int pages;
     String publisher;
     String isbn;
+    String binding;
+
+    Book(String title, String author, int releaseDate, int pages, String publisher, String binding, String isbn){
+        this(title, author, releaseDate, pages, publisher, binding);
+        this.isbn = isbn;
+    }
+    Book(String title, String author, int releaseDate, int pages, String publisher, String binding){
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pages = pages;
+        this.publisher = publisher;
+        this.binding  = binding;
+    }
+
+    void printInfo(){
+        String info = title+" "+author+" "+releaseDate+" "+pages+" "+publisher+" "+binding+" "+isbn;
+        System.out.println(info);
+    }
+
 }
--- src/Library.java
+++ src/Library.java
@@ -2,22 +2,28 @@ public class Library {
     public static void main(String[] args) {
         final String appName = "Biblioteka";
 
-        Book book1 = new Book();
-        book1.title = "W pustyni i w puszczy";
-        book1.author = "Henryk Sienkiewicz";
-        book1.releaseDate = 2010;
-        book1.pages = 256;
-        book1.publisher = "Greg";
-        book1.isbn = "9788373278190";
+        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 256, "Greg","Miękka", "97883732781890");
+        Book book2 = new Book("Krzyżacy", "Henryk Sienkiewicz", 2018, 600, "Greg","Miękka", "9788373271821");
+        Book book3 = new Book("Zemsta", "Aleksander Fredro", 2018, 152, "Greg", "Miękka","9788373271968" );
+
+//        book1.title = "W pustyni i w puszczy";
+//        book1.author = "Henryk Sienkiewicz";
+//        book1.releaseDate = 2010;
+//        book1.pages = 256;
+//        book1.publisher = "Greg";
+//        book1.isbn = "9788373278190";
 
         System.out.println(appName);
         System.out.println("Książki w bibliotece");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
+//        System.out.println(book1.title);
+//        System.out.println(book1.author);
+//        System.out.println(book1.releaseDate);
+//        System.out.println(book1.pages);
+//        System.out.println(book1.publisher);
+//        System.out.println(book1.isbn);
 
     }
 }
